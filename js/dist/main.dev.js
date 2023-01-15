c17bf6be8f348c6fce61250466972eef970e72b7
yassinecheggar <cheggar13@gmail.com>
2023-01-15 20:55:15 +0100

    add block partenaires

--- js/dist/main.dev.js
+++ js/dist/main.dev.js
@@ -20,3 +20,20 @@
     });
   });
 })(jQuery);
+
+$(".brand-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  autoplay: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 3
+    },
+    1000: {
+      items: 5
+    }
+  }
+});
--- js/main.js
+++ js/main.js
@@ -18,3 +18,20 @@
     });
   });
 })(jQuery);
+
+$(".brand-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  autoplay: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 5,
+    },
+  },
+});
