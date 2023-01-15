fefa91554b9107c10bafcf66d1cd586957a32364
yassinecheggar <cheggar13@gmail.com>
2023-01-15 01:33:26 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+(function ($) {
+  $(function () {
+    $("nav ul li > a:not(:only-child)").click(function (e) {
+      $(this).siblings(".nav-dropdown").slideToggle();
+      $(".nav-dropdown").not($(this).siblings()).hide();
+      e.stopPropagation();
+    });
+    $("html").click(function () {
+      $(".nav-dropdown").hide();
+    });
+    // Toggle open and close nav styles on click
+    $("#nav-toggle").click(function () {
+      $("nav ul").slideToggle();
+      $(".icon-container").toggleClass("hidden-container");
+    });
+    $("#nav-toggle").on("click", function () {
+      this.classList.toggle("active");
+    });
+  });
+})(jQuery);
